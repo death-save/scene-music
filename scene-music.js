a176a81a35e1fa4b5bf55f94d7678bdd9957830d
errational <42503461+eclarke12@users.noreply.github.com>
2019-10-23 13:29:13 -0700

    Create scene-music.js

---
+++ scene-music.js
@@ -0,0 +1,5 @@
+//create flag on scene entity to store a playlist id
+
+//add a dropdown to the scene config to select the playlist
+
+//hook on scene update and if it's active and has the flag set, play the playlist
